1e3579022cdeb9396659529384beef1c34e1284e
abayomi igwubor <igwubor@outlook.com>
2020-06-07 19:29:43 +0100

    added Routing

--- BikeApplication/Controllers/MakeController.cs
+++ BikeApplication/Controllers/MakeController.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BikeApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BikeApplication.Controllers
 {
     public class MakeController : Controller
     {
-        public IActionResult Index()
+        //Make
+        //Make/Bikes
+        [Route("Make")]
+        [Route("Make/Bikes")]
+        public IActionResult Bikes()
         {
-            return View();
+            Make make = new Make { Id = 1, Name = "Harley Davidson" };
+            return View(make);
         }
     }
 }
