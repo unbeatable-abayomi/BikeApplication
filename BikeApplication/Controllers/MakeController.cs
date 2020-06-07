c407d378e99c45da973d3c0fbf9cff07934ff584
abayomi igwubor <igwubor@outlook.com>
2020-06-07 22:00:38 +0100

    added EditFunctionality

--- BikeApplication/Controllers/MakeController.cs
+++ BikeApplication/Controllers/MakeController.cs
@@ -39,6 +39,48 @@ namespace BikeApplication.Controllers
             }
             return View(make);
         }
+
+        [HttpPost]
+        public IActionResult Delete(int Id)
+        {
+            var make = _db.Makes.Find(Id);
+            if(make == null)
+            {
+                return NotFound();
+            }
+            _db.Makes.Remove(make);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+        public IActionResult Edit(int Id)
+        {
+            var make = _db.Makes.Find(Id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            return View(make);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                //_db.Makes.Add(make);
+                _db.Makes.Update(make);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(make);
+        }
+        //[HttpPost]
+        //public IActionResult Delete()
+        //{
+        //    return View();
+        //}
         //Make
         //Make/Bikes
         //[Route("Make")]
